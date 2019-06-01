a92ea6718bd4719aeddb09368b63559070f9b00c
Ken Yu <shinabro@gmail.com>
2019-06-01 15:39:32 -0500

    In-Memory Auth and Authorization

---
+++ project-start/src/main/java/edu/depaul/cdm/se452/demo/config/SecurityConfig.java
@@ -0,0 +1,58 @@
+package edu.depaul.cdm.se452.demo.config;
+
+import edu.depaul.cdm.se452.demo.security.LoggingAccessDeniedHandler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private LoggingAccessDeniedHandler accessDeniedHandler;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                    .antMatchers(
+                            "/",
+                            "/js/**",
+                            "/css/**",
+                            "/img/**",
+                            "/webjars/**").permitAll()
+                    .antMatchers("/airport/**").hasRole("USER")
+                    .anyRequest().authenticated()
+                .and()
+//                .httpBasic()
+                .formLogin()
+                    .loginPage("/login")
+                    .permitAll()
+                .and()
+                .logout()
+                    .invalidateHttpSession(true)
+                    .clearAuthentication(true)
+                    .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                    .logoutSuccessUrl("/login?logout")
+                    .permitAll()
+                .and()
+                .exceptionHandling()
+                    .accessDeniedHandler(accessDeniedHandler);
+
+    }
+
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        auth
+                .inMemoryAuthentication()
+                .withUser("reg-user").password(encoder.encode("password")).roles("USER")
+                .and()
+                .withUser("admin-user").password(encoder.encode("password")).roles("ADMIN");
+    }
+}
