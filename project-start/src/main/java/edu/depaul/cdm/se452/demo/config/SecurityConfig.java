73432a5db47bafd6087072e5d0c66e34935e235c
Ken Yu <shinabro@gmail.com>
2019-06-01 22:14:12 -0500

    In Memory and DB

--- project-start/src/main/java/edu/depaul/cdm/se452/demo/config/SecurityConfig.java
+++ project-start/src/main/java/edu/depaul/cdm/se452/demo/config/SecurityConfig.java
@@ -2,10 +2,14 @@ package edu.depaul.cdm.se452.demo.config;
 
 import edu.depaul.cdm.se452.demo.security.LoggingAccessDeniedHandler;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
@@ -20,39 +24,54 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                    .antMatchers(
-                            "/",
-                            "/js/**",
-                            "/css/**",
-                            "/img/**",
-                            "/webjars/**").permitAll()
-                    .antMatchers("/airport/**").hasRole("USER")
-                    .anyRequest().authenticated()
+                .antMatchers("/airport/**").hasRole("USER")
+                .anyRequest().permitAll()
                 .and()
-//                .httpBasic()
+                //                .httpBasic()
                 .formLogin()
-                    .loginPage("/login")
-                    .permitAll()
+                .loginPage("/login")
+                .permitAll()
                 .and()
                 .logout()
-                    .invalidateHttpSession(true)
-                    .clearAuthentication(true)
-                    .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-                    .logoutSuccessUrl("/login?logout")
-                    .permitAll()
+                .invalidateHttpSession(true)
+                .clearAuthentication(true)
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .logoutSuccessUrl("/login?logout")
+                .permitAll()
                 .and()
                 .exceptionHandling()
-                    .accessDeniedHandler(accessDeniedHandler);
+                .accessDeniedHandler(accessDeniedHandler);
 
+        http.csrf().disable();
+        http.headers().frameOptions().disable();
+        
     }
 
     @Override
     public void configure(AuthenticationManagerBuilder auth) throws Exception {
         PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
         auth
-                .inMemoryAuthentication()
-                .withUser("reg-user").password(encoder.encode("password")).roles("USER")
-                .and()
-                .withUser("admin-user").password(encoder.encode("password")).roles("ADMIN");
+                .authenticationProvider(dbAuthenticationProvider());
+//                .inMemoryAuthentication()
+//                .withUser("reg-user").password(encoder.encode("password")).roles("USER")
+//                .and()
+//                .withUser("admin-user").password(encoder.encode("password")).roles("ADMIN");
+    }
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    
+    public DaoAuthenticationProvider dbAuthenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService);
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+    }
+
+  
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
---
+++ project-start/src/main/java/edu/depaul/cdm/se452/demo/security/AuthorityType.java
@@ -0,0 +1,6 @@
+package edu.depaul.cdm.se452.demo.security;
+
+public enum AuthorityType {
+    ROLE_ADMIN,
+    ROLE_USER    
+}
